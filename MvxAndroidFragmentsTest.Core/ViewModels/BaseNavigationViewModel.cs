148f1eaf5a6a21cb2a948ca084d239bed16ac9ab
Daniele Ardissone <daniele.ardissone@yahoo.com>
2016-10-10 16:00:34 +0200

    Update BaseNavigationViewModel.cs

--- MvxAndroidFragmentsTest.Core/ViewModels/BaseNavigationViewModel.cs
+++ MvxAndroidFragmentsTest.Core/ViewModels/BaseNavigationViewModel.cs
@@ -8,35 +8,26 @@ using MvxAndroidFragmentsTest.Core.Constants;
 using MvxAndroidFragmentsTest.Core.Helpers;
 using MvvmCross.Core.ViewModels;
 using MvxAndroidFragmentsTest.Core.Exceptions;
-using MvxAndroidFragmentsTest.Core.Services;
 using MvxAndroidFragmentsTest.Core.Data;
 
 namespace MvxAndroidFragmentsTest.Core.ViewModels
 {
 	public class BaseNavigationViewModel : BaseViewModel
 	{
-		private IProfileService _profileSvc = null;
-		private IMenuService _menuSvc = null;
-
 		public BaseNavigationViewModel ()
 		{
 
 		}
 
-		public BaseNavigationViewModel (IProfileService profileService)
+		public BaseNavigationViewModel ()
 		{
-			_profileSvc = Mvx.Resolve<IProfileService> ();
 		}
 
 		#region Commands
-		//public virtual IMvxCommand SyncCommand { get { return null; } }
-
 		public IMvxCommand LogoutCommand { get { return new MvxCommand (DoLogout); } }
 
 		private void DoLogout()
 		{
-			//Action<bool> onConfirm = new Action<bool> (Confirm);
-
 			try
 			{
 				ShowConfirmMessageBox(CoreResources.confirm_logout_title, String.Empty, new Action<bool> (ConfirmLogout));
@@ -49,63 +40,7 @@ namespace MvxAndroidFragmentsTest.Core.ViewModels
 		}
 
 		public IMvxCommand RefreshNotificationsCommand { get { return new MvxCommand (DoRefreshNotifications); } }
-
-		private MvxCommand<Notification> _notificationSelectedCommand;
-		public IMvxCommand NotificationSelectedCommand
-		{
-			get
-			{
-				_notificationSelectedCommand = _notificationSelectedCommand ?? new MvxCommand<Notification>(MarkNotificationAsRead);
-				return _notificationSelectedCommand;
-			}
-		}
-		//public IMvxCommand ShowNotificationsCommand { get { return new MvxCommand (DoShowNotifications); } }
-
-		public virtual IMvxCommand SyncCommand { get { return new MvxCommand (DoSync); } }
-		private async void DoSync()
-		{
-			_menuSvc = Mvx.Resolve<IMenuService> ();
-
-			try
-			{
-				CheckDeviceConnectivity();
-
-				bool success = await ShowLoadingForLongOperationAsync<bool>(async () => 
-					{
-						await _menuSvc.SyncAsync(Settings.LastUsername, Settings.LastPassword);
-						LastSync = UpdateLastSync();
-
-						return true;
-					}
-				);
-			}
-			catch (MvxAndroidFragmentsTestOfflineException ex) {
-				RaiseGeneralFailEvent (ex);
-			}
-			catch (WebException ex) {
-				RaiseGeneralFailEvent (ex);
-			}
-			catch (Exception ex) {
-				RaiseGeneralFailEvent(ex);
-			}
-		}
 		#endregion
-
-		protected virtual string UpdateLastSync()
-		{
-			Customer cust = _menuSvc.GetCustomer();
-			return cust != null ? cust.BusinessName + " - Ultima sincronizzazione: " + SyncLog.ReadTimeLog(ZumeroSync.NOVUSMOBILE_TITLE) : "Ultima sincronizzazione: " + SyncLog.ReadTimeLog(ZumeroSync.NOVUSMOBILE_TITLE);
-			//return "Ultima sincronizzazione " + SyncLog.ReadTimeLog (ZumeroSync.NAVSERVICE_TITLE);
-		}
-
-		private void RemoveDB()
-		{
-			IMvxFileStore _fileStore = Mvx.Resolve<IMvxFileStore>();
-
-			var pathComposed = _fileStore.PathCombine(ZumeroSync.DB_STANDARD_PATH, ZumeroSync.DB_NAME);
-			if (_fileStore.Exists (pathComposed))
-				_fileStore.DeleteFile (pathComposed);
-		}
 
 		private async void ConfirmLogout(bool flag)
 		{
@@ -113,15 +48,8 @@ namespace MvxAndroidFragmentsTest.Core.ViewModels
 			{
 				try
 				{
-					_profileSvc = Mvx.Resolve<IProfileService> ();
+					ShowViewModelAndClearBackStack<LoginViewModel> ();
 
-					bool success = await ShowLoadingForLongOperationAsync<bool>(async () => await _profileSvc.LogoutAsync());
-
-					if (success)
-						ShowViewModelAndClearBackStack<LoginViewModel> ();
-					else
-						RaiseGeneralFailEvent(new MolloOfficinaException(
-							CoreResources.err_logout));
 				}
 				catch (MvxAndroidFragmentsTestOfflineException ex) {
 					RaiseGeneralFailEvent (ex);
